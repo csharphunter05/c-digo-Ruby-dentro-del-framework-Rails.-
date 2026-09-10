03c79b833a2579e79128be7a6319b986972b2f9a
IngTiburon <miguelmunguia050605@gmail.com>
2026-09-09 21:05:07 -0600

    tabla hash

--- app/app/controllers/hash_controller.rb
+++ app/app/controllers/hash_controller.rb
@@ -1,4 +1,10 @@
 class HashController < ApplicationController
   def mostrar
+       @persona = {
+      nombre: "Mario",
+      apellido: "Pérez",
+      telefono: "8922177",
+      correo: "Mario@gmail.com"
+    }
   end
 end
